6be57f2e910e131b4b4c2cade6460bd1502ad08f
Patrik Duksin <duksin2018@icloud.com>
2021-08-22 04:54:59 +0300

    Initial Commit

---
+++ mobileUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobileUp
+//
+//  Created by Patrik Duksin on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
